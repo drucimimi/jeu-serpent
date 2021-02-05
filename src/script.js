dc257f3372c3f12422a8c16894b6ac65cb02cb28
Drucilla Deroche <drucimimi@protonmail.com>
2021-02-05 13:34:18 +0100

    Update files

--- src/script.js
+++ src/script.js
@@ -5,46 +5,43 @@ import Game from "./game.js";
 /*Au chargement de la page, le jeu se lance seulement depuis un ordinateur*/
 window.onload = () => {
 
-    function newContent(){
-        let screenSize = window.screen.width;
-        if (screenSize <= 1024){
-          document.open();
-          document.write("<p>Le site n'est pas compatible avec les appareils mobiles.</p>");
-          document.close(); 
-        }  
-    }
+    let screenSize = window.screen.width;
+    if (screenSize <= 1024){
+        document.open();
+        document.write("<p>Le site n'est pas compatible avec les appareils mobiles.</p>");
+        document.close(); 
+    } else {
+        let myGame = new Game();
+        myGame.init();
 
-    let myGame = new Game();
-    myGame.init();
-
-    /*Diriger le serpent avec les flèches du clavier*/
-    document.onkeydown = (e) => {
-        const key = e.key;
-        let newDirection;
-        switch(key){
-            case "Left":
-            case "ArrowLeft":    
-                newDirection = "left";
-                break;
-            case "Up":
-            case "ArrowUp":   
-                newDirection = "up";
-                break;
-            case "Right":
-            case "ArrowRight":
-                newDirection = "right";
-                break;
-            case "Down":
-            case "ArrowDown":
-                newDirection = "down";
-                break;
-            case "Enter":
-                myGame.launch();
-                return;
-            default:
-                return;
-        }
-        myGame.snakee.setDirection(newDirection);
-    };
-
+        /*Diriger le serpent avec les flèches du clavier*/
+        document.onkeydown = (e) => {
+            const key = e.key;
+            let newDirection;
+            switch(key){
+                case "Left":
+                case "ArrowLeft":    
+                    newDirection = "left";
+                    break;
+                case "Up":
+                case "ArrowUp":   
+                    newDirection = "up";
+                    break;
+                case "Right":
+                case "ArrowRight":
+                    newDirection = "right";
+                    break;
+                case "Down":
+                case "ArrowDown":
+                    newDirection = "down";
+                    break;
+                case "Enter":
+                    myGame.launch();
+                    return;
+                default:
+                    return;
+            }
+            myGame.snakee.setDirection(newDirection);
+        };
+    } 
 }
